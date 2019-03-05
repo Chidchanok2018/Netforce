a032014523149bdf95ae047d5f77916f49b57ef1
Permpoon Chaowanaphunphon <38369861+mossnana@users.noreply.github.com>
2019-03-05 17:18:05 +0700

    Add files via upload

--- day1/afternoon/afternoon_test2.py
+++ day1/afternoon/afternoon_test2.py
@@ -1,31 +1,9 @@
-products={
-        "001": "PC",
-        "002": "Notebook",
-        "003": "Mouse",
-}
+c = []
+for i in range(20):
+    if i%3 == 0:
+        c.append(i)
+    else:
+        c.append(i-1)
 
-product = ["Keyboard", "PC", "Notebook", "Mouse"]
-
-shops = {
-        "shop1": {3: 500, 0: 100},
-        "shop2": {2: 30, 1: 20},
-        "shop3": {1: 55, 3: 44},
-        "shop4": {1: 50, 3: 60},
-        "shop5": {3: 500, 0: 100},
-}
-
-
-f = 1
-for keyMain, value in shops.items():
-	for key, value in value.items():
-		if f%2 != 0:
-			valueIndex = key
-			# print("{} has {} {} PCE and ".format(keyMain, product[valueIndex], value))
-			string1 = "{} has {} {} PCE and ".format(keyMain, product[valueIndex], value)
-		else:
-			valueIndex = key
-			# print("{} {} PCE".format(product[valueIndex], value))	
-			string2 = "{} {} PCE".format(product[valueIndex], value)
-		f = f + 1
-	print("" + string1 + string2)
+print(c)
 
